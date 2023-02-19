d945b4873898c80c5102acb401e1af3ee25d23e6
Jamshed Javed <jamshady@gmail.com>
2023-02-19 20:05:18 +0000

    Implement jsdom for response parsing into a DOM

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const jsdom = require('jsdom')
+
 fetch('https://wltest.dns-systems.net')
-    .then(response => {
-        console.log(response)
+    .then(response => response.text())
+    .then(body => {
+        const virtualConsole = new jsdom.VirtualConsole();
+        virtualConsole.on('error', () => {}); // prevent console errors from parsing CSS, etc
+        return new jsdom.JSDOM(body, { virtualConsole });
+    })
+    .then(dom => {
+        console.log(dom.window.document.getElementsByClassName('package').length)
     })
     .catch(err => console.error(err))
